5bc84a9eef6094ba0a5702e8c26745fafa5838ad
erdeszdonat <erdeszdonatcd@gmail.com>
2026-04-08 11:29:27 +0200

    Update and rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
   async redirects() {
     return [
       // SZOLGÁLTATÁSOK - Régi URL-ek → Új struktúra
@@ -37,7 +44,6 @@ const nextConfig: NextConfig = {
       { source: '/cpg/:path*', destination: '/', permanent: false },
     ];
   },
-
   async rewrites() {
     return [
       { source: '/esztergom/:path*', destination: '/lokacio/esztergom/:path*' },
