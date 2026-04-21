1980587be2823826b16e01042a9cdea37506fd19
erdeszdonat <erdeszdonatcd@gmail.com>
2026-04-21 09:36:01 +0200

    Make compiler-runtime c() stable with useMemo to stop infinite loop

--- shims/compiler-runtime.js
+++ shims/compiler-runtime.js
@@ -1,9 +1,13 @@
+import { useMemo } from 'react';
+
 const MEMO_CACHE_SENTINEL = Symbol.for('react.memo_cache_sentinel');
 
 export function c(size) {
-  const arr = new Array(size);
-  for (let i = 0; i < size; i++) arr[i] = MEMO_CACHE_SENTINEL;
-  return arr;
+  return useMemo(() => {
+    const arr = new Array(size);
+    for (let i = 0; i < size; i++) arr[i] = MEMO_CACHE_SENTINEL;
+    return arr;
+  }, []);
 }
 
 export default { c };
