c241d8d0659bb2ff6e1a3de00ddfee18bf2e2179
erdeszdonat <erdeszdonatcd@gmail.com>
2026-04-21 15:39:20 +0200

    Add Google Ads AW-16510822421 and Meta Pixel tracking

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -132,8 +132,14 @@ export default async function LocaleLayout({ children, params }: Props) {
       />
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-9BS3P1DC4T" strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-9BS3P1DC4T');`}
+        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-9BS3P1DC4T');gtag('config','AW-16510822421');`}
       </Script>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1245020569792754');fbq('track','PageView');`}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1245020569792754&ev=PageView&noscript=1" alt="" />
+      </noscript>
       <NextIntlClientProvider locale={locale} messages={messages}>
         <ClientLayout>{children}</ClientLayout>
       </NextIntlClientProvider>
